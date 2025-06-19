1d8cd49db30000b60c177f9532a445520ce30089
Zidanngoding <151296002+Zidanngoding@users.noreply.github.com>
2025-06-19 09:38:45 +0700

    Update failytail.sql

--- database/failytail.sql
+++ database/failytail.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 18, 2025 at 09:59 AM
+-- Generation Time: Jun 19, 2025 at 02:38 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -41,7 +41,7 @@ CREATE TABLE `film` (
 --
 
 INSERT INTO `film` (`id`, `judul`, `genre`, `tahun_rilis`, `rating`, `komentar`) VALUES
-(3, 'haha', 'action', '1999', 10.0, 'bagus');
+(3, 'haha', 'action', 1999, '10.0', 'bagus');
 
 -- --------------------------------------------------------
 
@@ -62,11 +62,9 @@ CREATE TABLE `filmadmin` (
 --
 
 INSERT INTO `filmadmin` (`id`, `poster`, `judul`, `genre`, `deskripsi`) VALUES
-(2, '../image/posters/Kowaru Neon Genesis Evangelion Wallpaper.jpg', 'Neon Genesis Evangelion', 'Horror', 'spongebobbbb'),
-(3, '../image/posters/oppenheimer.jpg', 'Oppenheimer', 'Drama', 'deksripsi singkat'),
-(4, '../image/posters/Perfect blue (1997) [691x1024].jpg', 'Perfect Blue', 'Drama', 'apayaa'),
-(6, '../image/posters/Akira.jpeg', 'Akira', 'Action', 'Akira seorang ngabers'),
-(7, '../image/posters/the wind rises.jpeg', 'The Wind Rises', 'Romance', 'Seorang yang jatuh cinta');
+(2, '../poster/ttd.jpg', 'Flying Dutchman ', 'Horror', 'spongebobbbb'),
+(3, '../poster/ttd.jpg', 'Oppenheimer', 'action', 'deksripsi singkat'),
+(4, '../poster/wa.jpg', 'Perfect Blue', 'Drama', 'apayaa');
 
 -- --------------------------------------------------------
 
@@ -79,7 +77,7 @@ CREATE TABLE `ulasan` (
   `user_id` int NOT NULL,
   `film_id` int NOT NULL,
   `komentar` text NOT NULL,
-  `rating` decimal(2,1) NOT NULL,
+  `rating` float DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -88,8 +86,7 @@ CREATE TABLE `ulasan` (
 --
 
 INSERT INTO `ulasan` (`id`, `user_id`, `film_id`, `komentar`, `rating`, `created_at`) VALUES
-(2, 3, 7, 'Ini film ghibli paling romatis', 9.5, '2025-06-18 09:52:24'),
-(3, 3, 6, 'Keren banget action dari akira', 9.2, '2025-06-18 09:56:03');
+(1, 3, 3, 'bagus', 9.7, '2025-06-19 02:37:01');
 
 -- --------------------------------------------------------
 
@@ -101,7 +98,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `role` enum('admin','user') DEFAULT 'user',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(100) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
@@ -113,10 +110,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`, `email`, `phone`, `alamat`) VALUES
-(1, 'admin', '$2y$10$Wvq0pQ2XZ3y6K9JbLcRdR.7aB4cD5eF6gH7iJ8kL9mN0oP1qR2sT3u', 'admin', '2025-05-26 06:36:10', NULL, NULL, NULL),
+(1, 'admin', '$2y$10$ZFFWhePTaUpxhIe3CIXG2OqL3W938q2DkTy4ULsCdIsUFbev2i5re', 'admin', '2025-05-26 06:36:10', NULL, NULL, NULL),
 (2, 'user', '$2y$10$lk0eeWVYTAIHIDFLxkYBUOPWE7H.IPM6eZvQMupL2CClJhulvsquO', 'user', '2025-05-26 13:52:22', NULL, NULL, NULL),
-(3, 'zane', '$2y$10$0qyJhnQEwPipbcGPsswXLeSSdzyC.tWgKD8NgTZD3UA/J2eSIpw/G', 'admin', '2025-06-18 05:44:46', 'akbar.lamborgini@gmail.com', '081573049831', 'Jalan Kebersihan Gang Nurul Hidayah No 45'),
-(4, 'ilham', '$2y$10$OBO0MYpVZ.dNl0tpwMtoK.Y2UqfctvslSGYS3ySkENSa38MQoWSCy', 'user', '2025-06-18 06:14:07', 'nickle.ferum1135@gmail.com', '0872329840', 'Jalan Nasi Padang');
+(3, 'zidan', '$2y$10$OOOSIoxcBntfGrJ/DhoRBujUZIgELpWCTGWw8RUSRAWCStD99nKy6', 'user', '2025-06-19 02:36:26', 'zidan@gmail.com', '1234567890', 'rumah');
 
 --
 -- Indexes for dumped tables
@@ -163,19 +159,19 @@ ALTER TABLE `film`
 -- AUTO_INCREMENT for table `filmadmin`
 --
 ALTER TABLE `filmadmin`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `ulasan`
 --
 ALTER TABLE `ulasan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -185,8 +181,8 @@ ALTER TABLE `users`
 -- Constraints for table `ulasan`
 --
 ALTER TABLE `ulasan`
-  ADD CONSTRAINT `fk_ulasan_filmadmin` FOREIGN KEY (`film_id`) REFERENCES `filmadmin` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `ulasan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `ulasan_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ulasan_ibfk_2` FOREIGN KEY (`film_id`) REFERENCES `film` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
